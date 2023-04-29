0a6615e6be598e986d83bd9e037ef2f1cec218e6
Nimatu Alhassan <nalmat81@gmail.com>
2023-04-28 22:23:31 -0700

    sign

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,7 +5,7 @@
   *
   * Purpose - no hardcode
   * Return: (success)
-  */
+ */
 int main(void)
 {
 	unsigned long int i;
@@ -30,15 +30,15 @@ int main(void)
 	 aft1 = (aft / l);
 	 aft2 = (aft % l);
 
-	 for (i = 92; i < 99; ++i)
-	 {
+	for (i = 92; i < 99; ++i)
+	{
 		 printf(", %lu", aft1 + (aft2 / l));
 		 printf("%lu", aft2 % l);
 		 aft1 = aft1 + bef1;
 		 bef1 = aft1 - bef1;
 		 aft2 = aft2 + bef2;
 		 bef2 = aft2 - bef2;
-	 }
+	}
 	 printf("\n");
-	 return (0);
+	return (0);
 }
