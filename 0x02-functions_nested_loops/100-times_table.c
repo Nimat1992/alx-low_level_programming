9d055da496c3a329463dc7f356c8fb7567aabc7e
Nimatu Alhassan <nalmat81@gmail.com>
2023-04-28 23:35:39 -0700

    n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,8 +10,8 @@ void print_times_table(int n)
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
-{
-		       	for (j = 0; j <= n; j++)
+
+			for (j = 0; j <= n; j++)
 			{
 k = j * i;
 				if (j == 0)
@@ -44,7 +44,3 @@ k = j * i;
 		}
 	}
 }
-
-
-
-
