3e0c6925065332685abfe926446cbe9ba6ee45b2
Nimatu Alhassan <nalmat81@gmail.com>
2023-04-28 22:38:59 -0700

    a program that would steal passwords of students

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
-  * main - fibonacci <3
-  *
-  * Purpose - no hardcode
-  * Return: (success)
- */
+ * main - fibonacci <3
+ *
+ * Purpose - no hardcode
+ * Return: (success)
+*/
 int main(void)
 {
 	unsigned long int i;
@@ -20,25 +20,25 @@ int main(void)
 	printf("%lu", bef);
 
 	for (i = 1; i < 91; i++)
-	{
-		printf(", %lu", aft);
-		aft += bef;
-		bef = aft - bef;
-	}
-	 bef1 = (bef / l);
-	 bef2 = (bef % l);
-	 aft1 = (aft / l);
-	 aft2 = (aft % l);
+{
+	printf(", %lu", aft);
+	aft += bef;
+	bef = aft - bef;
+}
+	bef1 = (bef / l);
+	bef2 = (bef % l);
+	aft1 = (aft / l);
+	aft2 = (aft % l);
 
 	for (i = 92; i < 99; ++i)
-	{
-		 printf(", %lu", aft1 + (aft2 / l));
-		 printf("%lu", aft2 % l);
-		 aft1 = aft1 + bef1;
-		 bef1 = aft1 - bef1;
-		 aft2 = aft2 + bef2;
-		 bef2 = aft2 - bef2;
-	}
-	 printf("\n");
+{
+	printf(", %lu", aft1 + (aft2 / l));
+	printf("%lu", aft2 % l);
+	aft1 = aft1 + bef1;
+	bef1 = aft1 - bef1;
+	aft2 = aft2 + bef2;
+	bef2 = aft2 - bef2;
+}
+	printf("\n");
 	return (0);
 }
