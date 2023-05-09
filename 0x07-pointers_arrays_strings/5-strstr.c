c8252cb8ffc288bb6976ac7aff76801775f68407
Nimatu Alhassan <nalmat81@gmail.com>
2023-05-08 18:33:24 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,8 +8,8 @@
 char *_strstr(char *haystack, char *needle)
 {
 	for (; *haystack != '\0'; haystack++)
-{
-		char *l = haystack;
+	{
+	char *l = haystack;
 		char *p = needle;
 
 		while (*l == *p && *p != '\0')
@@ -20,5 +20,7 @@ char *_strstr(char *haystack, char *needle)
 
 		if (*p == '\0')
 		return (haystack);
-}
+	}
+
+	return (0);
 }
