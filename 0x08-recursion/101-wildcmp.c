283b569d684fc6f648f3181ae3a29c8505be0512
Nimatu Alhassan <nalmat81@gmail.com>
2023-05-10 15:24:55 -0700

    Inception. Now, before you bother telling me it's impossible

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
-
 /**
- * wildcmp - Comparing strings
+  * wildcmp - Compare strings
  * @s1: pointer to string params
  * @s2: pointer to string params
  * Return: 0
@@ -19,7 +18,7 @@ int wildcmp(char *s1, char *s2)
 
 	{
 
-	return (wildcmp(s1, s2 + 1));
+		return (wildcmp(s1, s2 + 1));
 
 	}
 
